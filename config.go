ff08ecca5754909596565b1918724ffe6b23703f
Nymphaea <mail@supaku.cn>
2024-09-07 02:27:44 +0800

    [feat]修改代码逻辑，以供使用Docker进行容器化部署
    Signed-off-by: Nymphaea <mail@supaku.cn>

--- config.go
+++ config.go
@@ -9,7 +9,7 @@ import (
 func LoadConfig() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("toml")
-	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config")
 
 	if err := viper.ReadInConfig(); err != nil {
 		logger.DPanic("Failed to read config file", zap.Error(err))
